1aa169eea9cf8c1b999439d7cd924c3a71353273
IreneOliveto <nene.ol@hotmail.it>
2022-09-15 15:03:19 +0200

    Update Employee.java

--- lab4.02/src/main/java/com/lab44/lab42/model/Employee.java
+++ lab4.02/src/main/java/com/lab44/lab42/model/Employee.java
@@ -1,5 +1,7 @@
 package com.lab44.lab42.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.util.List;
 
@@ -15,7 +17,7 @@ public class Employee {
     private Status status;
 
     @OneToMany(mappedBy="admittedBy")
-    /*@JsonIgnore*/
+    @JsonIgnore
     private List<Patient> patientList;
 
 
